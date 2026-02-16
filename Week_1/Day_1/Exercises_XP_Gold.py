e24c899467fca0524536039bc75e399455569b9b
almendraapolaya <almendraapolaya@gmail.com>
2026-02-16 14:19:58 +0200

    updated

---
+++ Week_1/Day_1/Exercises_XP_Gold.py
@@ -0,0 +1,20 @@
+# 
+#=========================================
+# Exercises XP Gold
+#-----------------------------------------
+#
+#=========================================
+# #Exercise 1 : Hello World-I love Python
+#=========================================
+# Instructions
+# Print the following output in one line of code:
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# I love python
+# I love python
+# I love python
+# I love python
+
+print(("Hello world\n"* 4) + ("I love python\n") * 4)
